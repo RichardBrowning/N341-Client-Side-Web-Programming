80a4c36d42d71963a856e4193355adc1f92223b4
RichardBrowning <mi888uzl@outlook.com>
2021-09-09 15:38:08 +0800

    first release of Multi Writes

--- multiWrites/js/multiWrites.js
+++ multiWrites/js/multiWrites.js
@@ -1,35 +1,55 @@
-
+/**************************************
+ TITLE: multiWrites.js
+ AUTHOR: Luxi Liao	(LL)	
+ PURPOSE: print message at the page
+ ORIGINALLY CREATED ON: 31 August 2021
+ LAST MODIFIED ON: 03 September 2021
+ LAST MODIFIED BY: Luxi Liao (LL)	
+ MODIFICATION HISTORY:
+ 31 August 2021 - Adding comment according to Requirement
+	and edit the function so that it alerts "Hello World!" (LL)
+ 03 September 2021 - Small changes according to directory re-structure (LL)
+***************************************/
 
 
 // A $( document ).ready() block.
 $(document).ready(
     function(){
-	// Pop up a window that says "Hello World!"
-	alert("Hello World");
+	//add content to div using textContent
+    var target = "textContentInput";
+    var message = "This part is inserted using textContent"
+    insertTextTextContent(target, content)
+    target = "innerHtmlInput";
+    message = "This part is inserted using innerHTML"
+    insertTextInnerHtml(target, content)
+    target = "docWriteInput";
+    message = "This part is inserted using document.write()"
+    insertTextDocWrite(target, content)
+},
+
+function parseTextTextContent(target){
+    // parsing content from the page
+    var strMessage = "";
+    var elOutput = document.getElementById(target); 
+    strMessage = elOutput.textContent; 
+},
+
+function insertTextTextContent(target, content){
+    //writing content to the page
+    var strMessage = content;
+    var elOutput = document.getElementById(target); 
+    elOutput.textContent = strMessage;
+},
+
+function insertTextInnerHtml(target, content){
+    var strMessage = content; 
+    var elOutput = document.getElementById(target); 
+    elOutput.innerHTML = strMessage;
+},
+
+function insertTextDocWrite(content){
+    document.write(content)
 }
 
 ); // end of $(document).ready()
 
-function parseTextTextContent(){
-    // parsing content from the page
-    var strMessage = "";
-    var elOutput = document.getElementById("textContentInput"); 
-    strMessage = elOutput.textContent; 
-};
-
-function insertTextTextContent(){
-    //writing content to the page
-    var strMessage = "Text we are writing to the page.";
-    var elOutput = document.getElementById("textContentInput"); 
-    elOutput.textContent = strMessage;
-}
-
-function insertTextInnerHtml(){
-    var strMessage = "Text we are <strong>writing</strong> to the page."; 
-    var elOutput = document.getElementById("myFavElementId"); 
-    elOutput.innerHTML = strMessage;
-}
-
-function insertTextDocWrite(){
-    document.write()
-}
