6bda2c7cc483c01ca6534abfbbbe00c925faef9d
RichardBrowning <mi888uzl@outlook.com>
2021-09-10 03:08:30 +0800

    final release of multi writes

--- multiWrites/js/multiWrites.js
+++ multiWrites/js/multiWrites.js
@@ -1,55 +1,109 @@
 /**************************************
  TITLE: multiWrites.js
  AUTHOR: Luxi Liao	(LL)	
- PURPOSE: print message at the page
- ORIGINALLY CREATED ON: 31 August 2021
- LAST MODIFIED ON: 03 September 2021
+ PURPOSE: print messages at the page
+ ORIGINALLY CREATED ON: 06 September 2021
+ LAST MODIFIED ON: 08 September 2021
  LAST MODIFIED BY: Luxi Liao (LL)	
  MODIFICATION HISTORY:
- 31 August 2021 - Adding comment according to Requirement
-	and edit the function so that it alerts "Hello World!" (LL)
- 03 September 2021 - Small changes according to directory re-structure (LL)
+ 07 September 2021 - Adding functions according to Requirement
+	to make page functional (LL)
+ 07 September 2021 - Attempts to move document.write() to a seperate JS file
+    failed (LL)
+ 08 September 2021 - Adding comment
+    to describe each function and some key statements (LL)
+ 08 September 2021 - Small changes according to directory re-structure (LL)
 ***************************************/
 
 
 // A $( document ).ready() block.
-$(document).ready(
-    function(){
+$(document).ready(function(){
+    /******************
+    NAME: parseTextTextContent
+    PURPOSE:	
+        parse content using textContent
+    PARAMETERS:
+        target Id
+    RETURN VALUE:
+        target Id's content
+    *******************/
+    $.fn.parseTextTextContent = function(target){ 
+        // parsing content from the page
+        // name variable for storage of content
+        var strMessage = "";
+        //get content by Id
+        var elOutput = document.getElementById(target); 
+        strMessage = elOutput.textContent; 
+        //return the variable, if empty, returns empty
+        return strMessage;
+    }
+    /******************
+    NAME: insertTextTextContent
+    PURPOSE:	
+        Print message using textContent
+    PARAMETERS:
+        target Id, desired content
+    RETURN VALUE:
+        void
+    *******************/
+    $.fn.insertTextTextContent = function(target, content){
+        //writing content to the page
+        //name variable for storage of content
+        var strMessage = content;
+        //get target for adding content
+        var elOutput = document.getElementById(target); 
+        //write content
+        elOutput.textContent = strMessage;
+    }
+    /******************
+    NAME: insertTextInnerHtml
+    PURPOSE:	
+        Print message using innerHTML
+    PARAMETERS:
+        target Id, desired content
+    RETURN VALUE:
+        void
+    *******************/
+    $.fn.insertTextInnerHtml = function(target, content){
+        //writing content to the page
+        //name variable for storage of content
+        var strMessage = content; 
+        //get target for adding content
+        var elOutput = document.getElementById(target); 
+        //write content
+        elOutput.innerHTML = strMessage;
+    }
+    /******************
+    NAME: insertTextDocWrite
+    PURPOSE:	
+        Print message using document.write
+    PARAMETERS:
+        desired content
+    RETURN VALUE:
+        void
+    *******************/
+    //This function will not have desired effect
+    $.fn.insertTextDocWrite = function(content){
+        //writing content to the page
+        document.open;
+        document.write(content);
+        document.close;
+    }
+
 	//add content to div using textContent
     var target = "textContentInput";
     var message = "This part is inserted using textContent"
-    insertTextTextContent(target, content)
+    $.fn.insertTextTextContent(target, message)
+    //add content to div using innerHTML, with changed content
     target = "innerHtmlInput";
-    message = "This part is inserted using innerHTML"
-    insertTextInnerHtml(target, content)
-    target = "docWriteInput";
-    message = "This part is inserted using document.write()"
-    insertTextDocWrite(target, content)
-},
-
-function parseTextTextContent(target){
-    // parsing content from the page
-    var strMessage = "";
-    var elOutput = document.getElementById(target); 
-    strMessage = elOutput.textContent; 
-},
-
-function insertTextTextContent(target, content){
-    //writing content to the page
-    var strMessage = content;
-    var elOutput = document.getElementById(target); 
-    elOutput.textContent = strMessage;
-},
-
-function insertTextInnerHtml(target, content){
-    var strMessage = content; 
-    var elOutput = document.getElementById(target); 
-    elOutput.innerHTML = strMessage;
-},
-
-function insertTextDocWrite(content){
-    document.write(content)
-}
-
-); // end of $(document).ready()
+    message = "<h3>This part is inserted using innerHTML</h3>"
+    $.fn.insertTextInnerHtml(target, message)
+    /*
+    this part of code is deprecated, 
+    remained here for reference
+    */
+    //target = "docWriteInput";
+    //message = "<div id=\"docWriteInput\">This part is inserted using document.write()</div>"
+    //$.fn.insertTextDocWrite(message)
+}); // end of $(document).ready()
 
