12debf1d971633941560a5d82cb743a1e36f1057
RichardBrowning <mi888uzl@outlook.com>
2021-09-30 14:41:32 +0800

    final commit of ifs

--- moreIfs/js/moreIfs.js
+++ moreIfs/js/moreIfs.js
@@ -30,80 +30,80 @@ $(document).ready(function(){
         intDestinationLatitude : 0, 
         //dest longitude
         intDestinationLongitude : 0,
+        //path
+        strPath : "",
 
         moveVector: {
-            intLatitudeChange: intDestinationLatitude - intCurrentLatitude, 
-            intLongitudeChange: intDestinationLongitude - intCurrentLongitude,
+            intLatitudeChange: 0,
+            intLongitudeChange: 0,
         },
 
         promptCoordinate : function() {
-            var arrayPrompt = ["What's your current latitude","What's your current longitude","What's your destination latitude","What's your destination longitude"];
-            var arrayVar = [this.intCurrentLatitude, this.intCurrentLongitude, this.intDestinationLatitude, this.intDestinationLongitude];
-            for (let i = 0; i < array.length; i++) {
-                arrayVar[i] = prompt(arrayPrompt[i]);
-            }
+            var arrayPrompt = ["What's your current latitude?", "What's your current longitude?", "What's your destination latitude?", "What's your destination longitude?"];
+            this.intCurrentLatitude = prompt(arrayPrompt[1])
+            this.intCurrentLongitude = prompt(arrayPrompt[2])
+            this.intDestinationLatitude = prompt(arrayPrompt[3])
+            this.intDestinationLongitude = prompt(arrayPrompt[4])
+            //console.log(this.intCurrentLatitude);//debug line
         },
         
         strPathIfElse : function(){
-            if (this.moveVector.intLatitudeChange > 0) {
-                if (this.moveVector.intLongitudeChange > 0) {
-                    
-                } else if(this.moveVector.intLatitudeChange < 0) {
-                    
+            if (this.moveVector.intLatitudeChange >= 0) {
+                if (this.moveVector.intLongitudeChange >= 0) {
+                    this.strPath = "Head North East";
+                } else if(this.moveVector.intLatitudeChange <= 0) {
+                    this.strPath = "Head North West";
                 } else {
-
+                    this.strPath="Land Ho!"
                 }
-            } else if(this.moveVector.intLatitudeChange < 0){
-                if (this.moveVector.intLongitudeChange > 0) {
-                    
-                } else if(this.moveVector.intLatitudeChange < 0) {
-                    
+            } else if(this.moveVector.intLatitudeChange <= 0){
+                if (this.moveVector.intLongitudeChange >= 0) {
+                    this.strPath = "Head South East";
+                } else if(this.moveVector.intLatitudeChange <= 0) {
+                    this.strPath = "Head Sorth West";
                 } else {
-                    
+                    this.strPath="Land Ho!";
                 }
             } else {
-                if (this.moveVector.intLongitudeChange > 0) {
-                    
-                } else if(this.moveVector.intLatitudeChange < 0) {
-                    
-                } else {
-                    //not moving
-                }
+                this.strPath="Land Ho!";
             }
         },
         strPathSwitch : function () {
             switch (true) {
-                case ():
-                    
+                case (this.moveVector.intLongitudeChange >= 0 && this.moveVector.intLongitudeChange >= 0):
+                    this.strPath = "Head North East";
                     break;
-                case ():
-                    
+                case (this.moveVector.intLongitudeChange >= 0 && this.moveVector.intLongitudeChange <= 0):
+                    this.strPath = "Head North West";
+                    break;     
+                case (this.moveVector.intLongitudeChange <= 0 && this.moveVector.intLongitudeChange >= 0):
+                    this.strPath = "Head South East";
                     break;
-                case ():
-                    
-                    break;         
-                case ():
-                    
-                    break;  
-                case ():
-                    
-                    break;
-                case ():
-                            
-                    break;
-                case ():
-                            
-                    break;         
-                case ():
-                            
-                    break;  
-                case ():
-
-                    break;                                 
+                case (this.moveVector.intLongitudeChange <= 0 && this.moveVector.intLongitudeChange <= 0):
+                    this.strPath = "Head South West";
+                    break;                        
                 default:
+                    this.strPath="Land Ho!";
                     break;
             }
+        },
+        strPrint : function(strElement){
+            var strElOutput = document.getElementById(strElement);
+            strElOutput.textContent = this.strPath;
+
         }
     }
+    Prompter.promptCoordinate();
+    Prompter.strPath = "Current Latitude: " + Prompter.intCurrentLatitude + "   CurrentLongitude: "+ Prompter.intCurrentLongitude + "    DestinationLatitude: " + Prompter.intDestinationLatitude + "    DestinationLongitude: " + Prompter.intDestinationLongitude;
+    Prompter.moveVector.intLatitudeChange = Prompter.intDestinationLatitude - Prompter.intCurrentLatitude;
+    Prompter.moveVector.intLongitudeChange = Prompter.intDestinationLongitude - Prompter.intCurrentLongitude;
+    Prompter.strPrint("echo")
+    //console.log(Prompter.strPath);//debug line
+    
+    Prompter.strPathIfElse();
+    Prompter.strPrint("ifElse");
+    Prompter.strPathSwitch();
+    Prompter.strPrint("caseSwitch")
+    
 }); // end of $(document).ready()
 
