d747eec0a73c79a9f3836065217f401c66a6c294
RichardBrowning <mi888uzl@outlook.com>
2021-11-07 08:06:42 +0800

    final release of m&s

--- moversAndShakers/js/moversAndShakers.js
+++ moversAndShakers/js/moversAndShakers.js
@@ -11,8 +11,6 @@
 *******************************/
 
 $(document).ready(function(){
-    //global variable: element of animation
-    var elActor = $('#actor');
         /******************
         NAME: showActor
         PURPOSE:	
@@ -22,8 +20,9 @@ $(document).ready(function(){
         RETURN VALUE:
             none, but show animation
         *******************/
-        function showActor() {
-			elActor.show(1500);
+        function showActor(event) {
+            var intSpeed = event.data.speed;
+			event.data.element.show(intSpeed);
 		}
         /******************
         NAME: hideActor
@@ -34,11 +33,12 @@ $(document).ready(function(){
         RETURN VALUE:
             none, but show animation
         *******************/
-		function hideActor() {
-			elActor.hide(1500);
+		function hideActor(event) {
+            var intSpeed = event.data.speed;
+			event.data.element.hide(intSpeed);
 		}
         /******************
-        NAME: strPathSwitch
+        NAME: toggleActor
         PURPOSE:	
             Switch case version of judging when direction to move
         PARAMETERS:
@@ -46,9 +46,31 @@ $(document).ready(function(){
         RETURN VALUE:
             none but set a string to describe direction
         *******************/
-        
+        function toggleActorSpeed(event) {
+            var intSpeed = event.data.speed;
+            event.data.element.toggle(intSpeed);
+        }
         /******************
-        NAME: parseTextTextContent
+        NAME: toggleActor
+        PURPOSE:	
+            Switch case version of judging when direction to move
+        PARAMETERS:
+            none
+        RETURN VALUE:
+            none but set a string to describe direction
+        *******************/
+        function toggleActorSwitch(event) {
+            var elActor = event.data.element;
+            
+            if (elActor.is(":visible")){
+                elActor.toggle(false);
+            } else {
+                elActor.toggle(true);
+            }
+            
+        }
+        /******************
+        NAME: slideUpActor
         PURPOSE:	
             print to certain element of the HTML page
         PARAMETERS:
@@ -56,9 +78,93 @@ $(document).ready(function(){
         RETURN VALUE:
             print content to target Id 
         *******************/
-		
-	//Register events for Buttons
-	$('button#show').click(showActor);
-	$('button#hide').click(hideActor);
-    
+        function slideUpActor(event) {
+            var intSpeed = event.data.speed;
+            event.data.element.slideUp(intSpeed);
+        }
+        /******************
+        NAME: slideDownActor
+        PURPOSE:	
+            print to certain element of the HTML page
+        PARAMETERS:
+            target Id
+        RETURN VALUE:
+            print content to target Id 
+        *******************/
+            function slideDownActor(event) {
+                var intSpeed = event.data.speed;
+                event.data.element.slideDown(intSpeed);
+            }
+        /******************
+        NAME: fadeInActor
+        PURPOSE:	
+            print to certain element of the HTML page
+        PARAMETERS:
+            target Id
+        RETURN VALUE:
+            print content to target Id 
+        *******************/
+            function fadeInActor(event) {
+                var intSpeed = event.data.speed;
+                event.data.element.fadeIn(intSpeed);
+            }
+        /******************
+        NAME: fadeOutActor
+        PURPOSE:	
+            print to certain element of the HTML page
+        PARAMETERS:
+            target Id
+        RETURN VALUE:
+            print content to target Id 
+        *******************/
+            function fadeOutActor(event) {
+                var intSpeed = event.data.speed;
+                event.data.element.fadeOut(intSpeed);
+            }
+        /******************
+        NAME: fadeOutActor
+        PURPOSE:	
+            print to certain element of the HTML page
+        PARAMETERS:
+            target Id
+        RETURN VALUE:
+            print content to target Id 
+        *******************/
+            function resetActor(event) {
+                var elActor = event.data.element;
+                elActor.show(450).css('position','absolute');
+            }
+
+    /******************
+        NAME: none, IIFE
+        PURPOSE:	
+            to capsulate
+        PARAMETERS:
+            none
+        RETURN VALUE:
+            none
+    *******************/
+	(function() {
+        //global variable: element of animation
+        var elActor = $('#actor');
+        /*Register events for Buttons*/
+        //show
+        $('button#show').on('click',{element: elActor, speed: 1500},showActor);
+        //hide
+        $('button#hide').on('click',{element: elActor, speed: 1200},hideActor);
+        //toggle
+        $('button#toggleSpeed').on('click',{element: elActor, speed: 1000},toggleActorSpeed);//multiple parameters for one function call
+        $('button#toggleSwitch').on('click',{element: elActor},toggleActorSwitch);
+
+        //slide
+        $('button#slideUp').on('click',{element: elActor, speed: 800},slideUpActor);
+        $('button#slideDown').on('click',{element: elActor, speed: 800},slideDownActor);
+        //fade
+        $('button#fadeIn').on('click',{element: elActor, speed: 1000}, fadeInActor);
+        $('button#fadeOut').on('click',{element: elActor, speed: 1000}, fadeOutActor);
+        
+        //reset
+        $('button#reset').on('click',{element: elActor}, resetActor);
+    }())
+
 }); // end of $(document).ready()
