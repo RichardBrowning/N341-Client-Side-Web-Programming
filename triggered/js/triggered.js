de0d1f916706fb68f3f49b9aed8d4b43249284bb
RichardBrowning <46521212+RichardBrowning@users.noreply.github.com>
2021-11-12 12:34:50 +0800

    Initiate Triggered

---
+++ triggered/js/triggered.js
@@ -0,0 +1,12 @@
+/**************************************
+* TITLE: triggered.js
+* AUTHOR: Luxi Liao (LL)
+* PURPOSE: Demostrate jQuery event listener
+* CREATE DATE: 10 November 2021
+* LAST MODIFIED ON: 10 November 2021
+* LAST MODIFIED BY: Luxi Liao (LL)
+* MODIFICATION HISTORY
+    10 November 2021 - Adding essential functions, design algorithms (LL)
+    10 November 2021 - implement Event Listeners and debugging (LL)
+**************************************/
+
